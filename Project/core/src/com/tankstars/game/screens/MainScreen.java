70bbfa31225cf838f2cd635864a61c7bbdedc3c1
Ankit Gautam <ankit21518@iiitd.ac.in>
2022-11-19 08:31:48 +0530

    mainMenu buttons added

--- Project/core/src/com/tankstars/game/screens/MainScreen.java
+++ Project/core/src/com/tankstars/game/screens/MainScreen.java
@@ -14,7 +14,7 @@ import com.tankstars.game.TankStars;
 public class MainScreen extends DefaultScreen{
     private Stage stage;
     private Table table;
-    private TextButton playButton, exitButton;
+    private TextButton playButton, exitButton, settingsButton;
     private Label heading;
     private Skin skin;
     private BitmapFont white,black;
@@ -25,7 +25,10 @@ public class MainScreen extends DefaultScreen{
     @Override
     public void show(){
         stage = new Stage();
-        atlas = new TextureAtlas("playbutton");
+
+        Gdx.input.setInputProcessor(stage);
+
+        atlas = new TextureAtlas("mainMenu/pack/button.atlas");
         skin = new Skin(atlas);
         table = new Table(skin);
         table.setBounds(0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
@@ -33,16 +36,24 @@ public class MainScreen extends DefaultScreen{
         black = new BitmapFont(Gdx.files.internal("fonts/black.fnt"), false);
 
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.getDrawable("playbutton.png");
-        textButtonStyle.down = skin.getDrawable("playbutton.png");
+        textButtonStyle.up = skin.getDrawable("button_up");
+        textButtonStyle.down = skin.getDrawable("button_down");
         textButtonStyle.pressedOffsetX = 1;
         textButtonStyle.pressedOffsetY = -1;
         textButtonStyle.font = black;
 
-        exitButton = new TextButton("EXIT",textButtonStyle);
+        playButton = new TextButton("PLAY KR VAI",textButtonStyle);
+        playButton.pad(20);
+
+        exitButton = new TextButton("Exit", textButtonStyle);
         exitButton.pad(20);
 
+        settingsButton = new TextButton("Settings", textButtonStyle);
+        settingsButton.pad(10);
+
+        table.add(playButton);
         table.add(exitButton);
+        table.add(settingsButton);
         table.debug();
         stage.addActor(table);
     }
@@ -50,10 +61,14 @@ public class MainScreen extends DefaultScreen{
     public void render(float delta){
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        game.resources.mainMenuBackgroundSprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        game.resources.mainMenuBackgroundSprite.draw(batch);
-        batch.end();
+
+        stage.setDebugAll(true);
+        stage.act(delta);
+        stage.draw();
+//        batch.begin();
+//        game.resources.mainMenuBackgroundSprite.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+//        game.resources.mainMenuBackgroundSprite.draw(batch);
+//        batch.end();
     }
     public void dispose(){
         batch.dispose();
