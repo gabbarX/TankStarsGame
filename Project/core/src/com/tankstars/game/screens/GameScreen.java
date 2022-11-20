bf55c91a4baf44b03a6c8d1e082facb967d5380f
Ankit Gautam <ankit21518@iiitd.ac.in>
2022-11-20 18:27:57 +0530

    Create GameScreen.java

---
+++ Project/core/src/com/tankstars/game/screens/GameScreen.java
@@ -0,0 +1,57 @@
+package com.tankstars.game.screens;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.tankstars.game.TankStars;
+
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen extends DefaultScreen {
+    public GameScreen(TankStars game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
