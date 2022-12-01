62fbc700a74c0ce0ef34d89949969667e27738c6
Prakhar-Gupta-03 <prakhar21550@iiitd.ac.in>
2022-12-01 23:31:52 +0530

    Uneven ground and tank 1
    Made an uneven ground and a tank body in the game screen. The forward button of player 1 doesn't work correctly right now (for testing purposes), which anyways will be deleted soon

--- Project/core/src/com/mygdx/game/screens/GameScreen.java
+++ Project/core/src/com/mygdx/game/screens/GameScreen.java
@@ -1,5 +1,6 @@
 package com.tankstars.game.screens;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -7,8 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -17,10 +17,10 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.InputController;
 import com.sun.java.swing.action.AlignCenterAction;
 import com.tankstars.game.Player;
 import com.tankstars.game.TankStars;
-import com.badlogic.gdx.physics.box2d.World;
 
 
 import com.badlogic.gdx.Screen;
@@ -32,31 +32,114 @@ public class GameScreen extends com.tankstars.game.screens.DefaultScreen {
     private World world;
     private Box2DDebugRenderer debugRenderer;
     private OrthographicCamera camera;
-    public GameScreen(TankStars game, int player1){
-        super(game);
-        flag = true;
-        this.isPlayer1 = player1;
-        this.isPlayer2 = (int) (Math.random() * 3);
-    }
-    public GameScreen(TankStars game, int player1, int player2) {
-        super(game);
-        flag = false;
-        this.isPlayer1 = player1;
-        this.isPlayer2 = player2;
-    }
     private Stage stage;
     private boolean flag;
     private Image background, dirtTerrain, healthBarP1, healthbarP2, badgeP1, badgeP2, vslogo;
     Player player1, player2;
     int isPlayer1, isPlayer2;
     private TextureAtlas tankAtlas;
-    @Override
-    public void show() {
+    public GameScreen(TankStars game, int player1){
+        super(game);
         world = new World(new Vector2(0,-9.8f), true);
         debugRenderer = new Box2DDebugRenderer();
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-
+        flag = true;
+        this.isPlayer1 = player1;
+        this.isPlayer2 = (int) (Math.random() * 3);
+        //  ground definition
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set(0, 0);
+        Body groundBody = world.createBody(groundBodyDef);
+        // ground shape
+        ChainShape groundShape = new ChainShape();
+        groundShape.createChain(new Vector2[]{
+                new Vector2(-600, -337.5f),
+                new Vector2(-600, 65),
+                new Vector2(-565, 65),
+                new Vector2(-515, 10),
+                new Vector2(-460, -55),
+                new Vector2(-325, -55),
+                new Vector2(-270, 10),
+                new Vector2(-245, 37),
+                new Vector2(-180, 37),
+                new Vector2(-155, 10),
+                new Vector2(-92, -64),
+                new Vector2(100, -64),
+                new Vector2(190, 38),
+                new Vector2(350, 38),
+                new Vector2(420, -35),
+                new Vector2(500, -35),
+                new Vector2(560, -110),
+                new Vector2(600, -110),
+                new Vector2(600, 337.5f),
+        });
+        // ground fixture
+        FixtureDef groundFixtureDef = new FixtureDef();
+        groundFixtureDef.shape = groundShape;
+        groundFixtureDef.density = 1f;
+        groundFixtureDef.friction = 0.4f;
+        groundFixtureDef.restitution = 0.6f;
+        Fixture groundFixture = groundBody.createFixture(groundFixtureDef);
+    }
+    public GameScreen(TankStars game, int player1, int player2) {
+        super(game);
+        world = new World(new Vector2(0,-9.8f), true);
+        debugRenderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        flag = false;
+        this.isPlayer1 = player1;
+        this.isPlayer2 = player2;
+        //  ground definition
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set(0, 0);
+        Body groundBody = world.createBody(groundBodyDef);
+        // ground shape
+        ChainShape groundShape = new ChainShape();
+        groundShape.createChain(new Vector2[]{
+                new Vector2(-600, -337.5f),
+                new Vector2(-600, 65),
+                new Vector2(-565, 65),
+                new Vector2(-515, 10),
+                new Vector2(-460, -55),
+                new Vector2(-325, -55),
+                new Vector2(-270, 10),
+                new Vector2(-245, 37),
+                new Vector2(-180, 37),
+                new Vector2(-155, 10),
+                new Vector2(-92, -64),
+                new Vector2(100, -64),
+                new Vector2(190, 38),
+                new Vector2(350, 38),
+                new Vector2(420, -35),
+                new Vector2(500, -35),
+                new Vector2(560, -110),
+                new Vector2(600, -110),
+                new Vector2(600, 337.5f),
+        });
+        // ground fixture
+        FixtureDef groundFixtureDef = new FixtureDef();
+        groundFixtureDef.shape = groundShape;
+        groundFixtureDef.density = 1f;
+        groundFixtureDef.friction = 0.4f;
+        groundFixtureDef.restitution = 0.6f;
+        Fixture groundFixture = groundBody.createFixture(groundFixtureDef);
+    }
+    @Override
+    public void show() {
+        // tank definition
+        BodyDef tankDef = new BodyDef();
+        tankDef.type = BodyDef.BodyType.DynamicBody;
+        tankDef.position.set(-400, -42);
+        Body tankBody = world.createBody(tankDef);
+        PolygonShape tankShape = new PolygonShape();
+        tankShape.setAsBox(20, 20);
+        // tank fixture definition
+        FixtureDef tankFixtureDef = new FixtureDef();
+        tankFixtureDef.shape = tankShape;
+        tankFixtureDef.density = 100f;
+        tankFixtureDef.friction = 0.4f;
+        tankFixtureDef.restitution = 0.6f;
+        Fixture tankFixture = tankBody.createFixture(tankFixtureDef);
         switch(isPlayer1) {
             case 0:
                 player1 = new Player("Abrams", true);
@@ -82,7 +165,7 @@ public class GameScreen extends com.tankstars.game.screens.DefaultScreen {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         tankAtlas = new TextureAtlas("Tanks/items.pack");
-        background = new Image(new Texture(Gdx.files.internal("Game Screen/main_game_bg.png")));
+        background = new Image(new Texture(Gdx.files.internal("Game Screen/gameScreenBackground.jpg")));
         dirtTerrain = new Image(new Texture(Gdx.files.internal("Game Screen/dirt_terrain.png")));
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
         textButtonStyle.font = new BitmapFont(Gdx.files.internal("Fonts/black.fnt"));
@@ -107,7 +190,8 @@ public class GameScreen extends com.tankstars.game.screens.DefaultScreen {
         forward1.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                player1.tank.setPosition(player1.tank.getPositionX()+10, player1.tank.getPositionY());
+                // go back to the previous screen using set screen
+                game.setScreen((Screen) new com.tankstars.game.screens.SelectTankScreen(game));
             }
         });
         backward1.addListener(new ClickListener() {
@@ -162,18 +246,22 @@ public class GameScreen extends com.tankstars.game.screens.DefaultScreen {
             stage.addActor(forward2);
             stage.addActor(backward2);
         }
+//        Gdx.input.setInputProcessor(new InputController(){
+//            @Override
+//            public boolean keyDown(int keycode){
+//                // if key is A
+//                if (keycode == Input.Keys.A){
+//                    // move tank 1 body to the left
+//
+//                }
+//            }
+//        })
     }
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        debugRenderer.render(world, camera.combined);
-        // create a body definition for the tank
-        BodyDef tankDef = new BodyDef();
-        tankDef.type = BodyDef.BodyType.DynamicBody;
-        tankDef.position.set(0, 0);
-        // create a fixture 
-//        stage.setDebugAll(true);
+        stage.setDebugAll(true);
         stage.act(delta);
         stage.draw();
         batch = new SpriteBatch();
@@ -181,6 +269,8 @@ public class GameScreen extends com.tankstars.game.screens.DefaultScreen {
         player1.tank.tankSprite.draw(batch);
         player2.tank.tankSprite.draw(batch);
         batch.end();
+        world.step(1/60f, 6, 2);
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
