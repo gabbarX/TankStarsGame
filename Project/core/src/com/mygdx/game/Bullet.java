b7ee3d9ade41baeacfb681990d49c46b4ed19565
Ankit Gautam <ankit21518@iiitd.ac.in>
2022-12-06 01:55:27 +0530

    Bullet class added

--- Project/core/src/com/mygdx/game/Bullet.java
+++ Project/core/src/com/mygdx/game/Bullet.java
@@ -1,35 +1,44 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Bullet
 {
-    private static int SPEED = 200;
-    private static int default_y = 40;
-    private static Texture texture;
-    float x,y;
-    public boolean remove = false;
+    Rectangle hitbox;
+    float a,time;
+    int speed;
+    Texture text;
 
-    public Bullet(float x){
-        this.x = x;
-        this.y = default_y;
-
-        if(texture==null){
-            texture = new Texture("MIRV.png");
-        }
-    }
-
-    public void update(float deltaTime){
-        y+= SPEED*deltaTime;
-        if(y> Gdx.graphics.getWidth()){
-            remove = true;
-        }
+    public Bullet(int x, int y, float angle) {
+        time = 2;
+        speed = 50;
+        hitbox = new Rectangle(x,y,10,10);
+        text = new Texture("Game Screen/tempBullet.png");
     }
 
 
-    public void render(SpriteBatch batch){
-        batch.draw(texture,x,y);
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public void update(float delta){
+        hitbox.x += speed*(float)Math.cos(a)*delta;
+        hitbox.y += speed*(float)Math.sin(a)*delta;
+        time -= delta;
+
+    }
+
+    public boolean isDead(){
+        if(time<0) return true;
+        else return false;
+    }
+
+    public void draw(SpriteBatch batch){
+        batch.draw(text,hitbox.x,hitbox.y,10,10);
+
     }
 }
+
+
